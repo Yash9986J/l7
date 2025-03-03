ffc347153abb2da72005b4f0562f93377355017a
Yash9986J <yashwanth.j@s.amity.edu>
2025-03-03 19:58:35 +0530

    size of int float

---
+++ size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Display the size of different data types
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of char: %zu byte\n", sizeof(char));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+
+    return 0;
+}
+
